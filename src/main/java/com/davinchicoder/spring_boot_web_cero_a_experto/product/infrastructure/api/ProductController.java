4a4b0baa43cfa3d9e05c1f4431522b80cdd667c2
David <david11jv@gmail.com>
2025-07-13 10:24:14 +0200

    Integrate OpenAPI support with Swagger UI:
    - Add `springdoc-openapi` and `spring-boot-starter-actuator` dependencies in `pom.xml`.
    - Configure API documentation with `OpenAPIConfig` including metadata and server details.
    - Annotate `ProductController` endpoints with OpenAPI `@Operation` and `@Tag` annotations.
    - Update `application.yml` to configure Swagger UI path.

--- src/main/java/com/davinchicoder/spring_boot_web_cero_a_experto/product/infrastructure/api/ProductController.java
+++ src/main/java/com/davinchicoder/spring_boot_web_cero_a_experto/product/infrastructure/api/ProductController.java
@@ -12,6 +12,8 @@ import com.davinchicoder.spring_boot_web_cero_a_experto.product.infrastructure.a
 import com.davinchicoder.spring_boot_web_cero_a_experto.product.infrastructure.api.dto.ProductDto;
 import com.davinchicoder.spring_boot_web_cero_a_experto.product.infrastructure.api.dto.UpdateProductDto;
 import com.davinchicoder.spring_boot_web_cero_a_experto.product.infrastructure.api.mapper.ProductMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +25,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@Tag(name = "Product", description = "Product API operations")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController implements ProductApi {
@@ -31,6 +34,7 @@ public class ProductController implements ProductApi {
 
     private final ProductMapper productMapper;
 
+    @Operation(summary = "Get all products", description = "Get all products")
     @GetMapping("")
     public ResponseEntity<List<ProductDto>> getAllProducts(@RequestParam(required = false) String pageSize) {
 
@@ -45,6 +49,7 @@ public class ProductController implements ProductApi {
         return ResponseEntity.ok(productDtos);
     }
 
+    @Operation(summary = "Get product by id", description = "Get product by id")
     @GetMapping("/{id}")
     public ResponseEntity<ProductDto> getProductById(@PathVariable Long id) {
 
@@ -59,6 +64,7 @@ public class ProductController implements ProductApi {
         return ResponseEntity.ok(productDto);
     }
 
+    @Operation(summary = "Save product", description = "Save product")
     @PostMapping("")
     public ResponseEntity<Void> saveProduct(@ModelAttribute @Valid CreateProductDto productDto) {
 
@@ -73,6 +79,7 @@ public class ProductController implements ProductApi {
         return ResponseEntity.created(URI.create("/api/v1/products/".concat(productDto.getId().toString()))).build();
     }
 
+    @Operation(summary = "Update product", description = "Update product")
     @PutMapping("")
     public ResponseEntity<Void> updateProduct(@ModelAttribute @Valid UpdateProductDto productDto) {
 
@@ -87,7 +94,7 @@ public class ProductController implements ProductApi {
         return ResponseEntity.noContent().build();
     }
 
-
+    @Operation(summary = "Delete product", description = "Delete product")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
 
